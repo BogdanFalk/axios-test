a977737d158b4d78bbb9beb6d7b445f5103b87f5
bogdan37falk <bogdan37falk@gmail.com>
2021-07-05 20:16:17 +0300

    axios-test

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import axios from "axios";
+import { useEffect } from "react";
 
 function App() {
+  useEffect(() => {
+    axios
+      .get(`https://strapi-todo-app.herokuapp.com/actions`)
+      .then((res) => console.log(res));
+
+    axios
+      .post(`https://strapi-todo-app.herokuapp.com/people`, {
+        email: "masdffsadfk@yologre.com",
+        password: "12356546",
+      })
+      .then((res) => console.log(res));
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
